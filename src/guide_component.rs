f26e682b498793f7c79fab9bd3e02cb22cf01dcd
Fabien BELLANGER <valentil@gmail.com>
2025-04-28 22:40:37 +0200

    WIP

--- src/guide_component.rs
+++ src/guide_component.rs
@@ -4,11 +4,18 @@ use dioxus::prelude::*;
 pub fn DogApp(breed: String) -> Element {
     rsx! {
         div {
-            class: "text-red-500 text-center cursor-pointer",
+            class: "text-gray-100 text-center cursor-pointer",
             onclick: move |_| {
                 info!("DogApp clicked");
             },
             "DogApp: breed = {breed}"
+        }
+        button {
+            class: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-2 rounded cursor-pointer",
+            onclick: move |_| {
+                info!("Button clicked");
+            },
+            "Click me"x
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use guide_component::DogApp;
 extern crate tracing;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/css/tailwind.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 fn main() {
     dioxus::logger::init(tracing::Level::DEBUG).expect("Failed to initialize logger");
@@ -20,29 +18,26 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Hero {}
-        DogApp { breed: "test".to_string() }
-    }
-}
+        document::Title { "Dioxus" }
 
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div { id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                    "💫 VSCode Extension"
-                }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+        document::Meta { name: "charset", content: "UTF-8" }
+        document::Meta {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+        }
+        document::Meta {
+            name: "description",
+            content: "Dioxus is a fast, portable, and ergonomic framework for building user interfaces.",
+        }
+        document::Meta { name: "keywords", content: "dioxus, rust, web, framework" }
+        document::Meta { name: "author", content: "Dioxus Labs" }
+
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        div { class: "flex flex-col items-center justify-center h-screen p-4 bg-gray-800",
+
+            DogApp { breed: "bulldog".to_string() }
         }
     }
 }
